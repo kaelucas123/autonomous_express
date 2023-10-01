45ed3ecd49dd5265c52c4a802ac1e50d36464dbc
Kae Lucas <kaeteodoro@gmail.com>
2023-10-01 00:00:13 -0300

    Home page

--- autonomous_express_ui/src/app/app.module.ts
+++ autonomous_express_ui/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './routes/landing-page/landing-page.component';
+import { HomeComponent } from './components/landing-page/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
