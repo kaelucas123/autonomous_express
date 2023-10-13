200a50e619a32ca659acaa7cb57027000e546056
kae lucas <kaeteodoro@gmail.com>
2023-10-12 21:04:55 -0300

    Criando Landing page

--- autonomous_express_ui/src/app/app.module.ts
+++ autonomous_express_ui/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import { LoginScreenComponent } from './routes/login-screen/login-screen.component';
 import {NgOptimizedImage} from "@angular/common";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import { MenuComponent } from './components/menu/menu.component';
 
 
 @NgModule({
@@ -15,14 +20,19 @@ import {NgOptimizedImage} from "@angular/common";
     AppComponent,
     LandingPageComponent,
     LoginScreenComponent,
+    MenuComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        NgOptimizedImage
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    NgOptimizedImage,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
